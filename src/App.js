d6e23b8ff13e8cead469dc265253403a17bd8827
macarenaromero <macarenasromero@gmail.com>
2022-08-03 20:19:42 -0300

    Pimera versión de footer y navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './styles/css/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       {/* CONTEXT (SI USAMOS) */}
 
- {/*      <BrowserRouter>
-        <Header />
+      
 
-        <Routes>
+      <BrowserRouter>
+        <Navbar/>
+
+        {/* <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/primera' element={<PrimeraPuesta />} />
+          <Route path='/primeraPuesta' element={<PrimeraPuesta />} />
           <Route path='/bebes' element={<Bebes />} />
-          <Route path='/niños' element={<Niños />} />
+          <Route path='/nenes' element={<Nenes />} />
           <Route path='/about' element={<About />} />
           <Route path='/help/:helpSection' element={<Help />} />
           <Route path="/*" element={<Error404 />} />
-        </Routes>
+        </Routes>*/}
 
-        <Footer />
-      </BrowserRouter> */}
+        <Footer /> 
+      </BrowserRouter>
 
 
     </>
---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import '../styles/css/Footer.css';
+
+const Footer = () => {
+
+    return (
+        <>
+            <div className='divider'></div>
+
+            <div className="footer">
+                <span className='row-links'>
+                    <div className='col'>
+                        <p className='title'>Conocenos</p>
+                        <Link className="btn" to='/about'>
+                            <p className="texto-btn">¿Quiénes somos?</p>
+                        </Link>
+                    </div>
+                    <div className='col'>
+                        <p className='title'>Ayuda</p>
+                        <Link className="btn" to='/help'>
+                            <p className="texto-btn">Recepción del pedido</p>
+                        </Link>
+                        <Link className="btn" to='/help'>
+                            <p className="texto-btn">Plazos y costos de envío</p>
+                        </Link>
+                        <Link className="btn" to='/help'>
+                            <p className="texto-btn">Cambios y devoluciones</p>
+                        </Link>
+                    </div>
+                    <div className='col'>
+                        <p className='title'>Atención al cliente</p>
+                        <p className="texto-btn">Teléfono: 094373344.</p>
+                        <p className="texto-btn">Horario de atención: Lunes a viernes de 10 a 18 hs.</p>
+                        <p className="texto-btn">Montevideo, Uruguay.</p>
+                    </div>
+                </span>
+
+                <div className='col-rrss'>
+                    <p className='title'>Seguinos en nuestras redes</p>
+                    <div className='container-rrss'>
+                        <Link className="btn" to=''>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg' alt='Logo de Instagram' className='logos-rrss'/>
+                        </Link>
+                        <Link className="btn" to=''>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png' alt='Logo de Facebook' className='logos-rrss'/>
+                        </Link>
+                        <Link className="btn" to=''>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/1/19/WhatsApp_logo-color-vertical.svg' alt='Logo de WhatsApp' className='logos-rrss'/>
+                        </Link>
+                        <Link className="btn" to=''>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg' alt='Logo de Gmail' className='logos-rrss'/>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
