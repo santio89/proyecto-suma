59fd8cc61f2967ee3bdf78e2c45a811a349a043d
santio89 <santiolais1989@gmail.com>
2022-08-04 14:32:22 -0300

    adding home

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import './styles/css/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/Home'
 
 function App() {
   return (
     <>
       {/* CONTEXT (SI USAMOS) */}
 
-      
-
       <BrowserRouter>
         <Navbar/>
 
-        {/* <Routes>
+        <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/primeraPuesta' element={<PrimeraPuesta />} />
+       {/*    <Route path='/primeraPuesta' element={<PrimeraPuesta />} />
           <Route path='/bebes' element={<Bebes />} />
           <Route path='/nenes' element={<Nenes />} />
           <Route path='/about' element={<About />} />
           <Route path='/help/:helpSection' element={<Help />} />
-          <Route path="/*" element={<Error404 />} />
-        </Routes>*/}
+          <Route path="/*" element={<Error404 />} /> */}
+        </Routes>
 
         <Footer /> 
       </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/css/index.css'
 import './styles/css/reset.css'
+import './styles/css/index.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
