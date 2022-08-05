84731d20812b71e4ca9c9e47f161f01620700962
santio89 <santiolais1989@gmail.com>
2022-08-05 11:09:12 -0300

    small fix folder name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './styles/css/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import Home from './Components/Home'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home'
 
 function App() {
   return (
