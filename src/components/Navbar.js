e56680cb0aa9c2eafcd0b7ba62a91055d1625aee
macarenaromero <macarenasromero@gmail.com>
2022-08-05 18:08:03 -0300

    navbar modifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,23 +10,25 @@ const Navbar = () => {
                     <h1 className="logo-btn">SUMA</h1>
                 </Link>
             </div>
-            <div className='link-btn'>
-                <Link className="btn" to='/primeraPuesta'>
-                    <p className="titulo-btn">PRIMERA PUESTA</p>
-                    <p>0 A 12 MESES</p>
-                </Link>
-            </div>
-            <div className='link-btn'>
-                <Link className="btn" to='/bebes'>
-                    <p className="titulo-btn">BEBÉS</p>
-                    <p>6 A 24 MESES</p>
-                </Link>
-            </div>
-            <div className='link-btn'>
-                <Link className="btn" to='/nenes'>
-                    <p className="titulo-btn">NIÑOS</p>
-                    <p>2 A 10 AÑOS</p>
-                </Link>
+            <div className='links'>
+                <div className='link-btn'>
+                    <Link className="btn" to='/primeraPuesta'>
+                        <p className="titulo-btn">PRIMERA PUESTA</p>
+                        <p>0 A 12 MESES</p>
+                    </Link>
+                </div>
+                <div className='link-btn'>
+                    <Link className="btn" to='/bebes'>
+                        <p className="titulo-btn">BEBÉS</p>
+                        <p>6 A 24 MESES</p>
+                    </Link>
+                </div>
+                <div className='link-btn'>
+                    <Link className="btn" to='/nenes'>
+                        <p className="titulo-btn">NIÑOS</p>
+                        <p>2 A 10 AÑOS</p>
+                    </Link>
+                </div>
             </div>
         </div>
     );
