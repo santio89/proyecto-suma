3e64a261dcc2713e7661d3e7f9b0b6e4f8c55966
santio89 <santiolais1989@gmail.com>
2022-08-10 12:32:10 -0300

    adding img modal

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,10 +1,25 @@
+import { useRef } from 'react';
 import '../styles/css/Producto.css'
 
-export default function Producto({nombre, url}) {
+export default function Producto({ nombre, url }) {
+    const imgModal = useRef();
+
+    const modalCloseClick = (e) => { if (e.target === imgModal.current) { imgModal.current.close(); window.removeEventListener("click", modalCloseClick) } };
+    const modalCloseScroll = () => { imgModal.current.close(); window.removeEventListener("scroll", modalCloseScroll) }
+
     return (
         <div className='producto'>
-            <div className='producto__imgContainer'><img alt={`img-${nombre}`} src={url}></img></div>
+            <button className='producto__imgContainer' onClick={() => {
+                imgModal.current.showModal();
+                window.addEventListener("click", modalCloseClick);
+                window.addEventListener("scroll", modalCloseScroll)
+            }}><img alt={`img-${nombre}`} src={url} loading="lazy"></img></button>
+            <i class="bi bi-arrows-angle-expand producto__expand"></i>
             <p className='producto__info'>{nombre}</p>
+            <dialog className='producto__imgModal' ref={imgModal}>
+                <button onClick={() => { imgModal.current.close() }}>X</button>
+                <img alt={`img-${nombre}`} src={url} loading="lazy"></img>
+            </dialog>
         </div>
     )
 }
