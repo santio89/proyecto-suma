d66e0e783252b78e1edbbebe3e18d8ea7f1c6346
santio89 <santiolais1989@gmail.com>
2022-08-22 22:17:08 -0300

    small fix

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -17,7 +17,7 @@ export default function Producto({ nombre, descripcion, url }) {
                     window.addEventListener("scroll", modalCloseScroll)
                 }}><img alt={`img-${nombre}`} src={url} loading="lazy"></img></button>
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" className="producto__expand" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="producto__expand" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z" />
                 </svg>
                 <p className='producto__name'>{nombre}</p>
